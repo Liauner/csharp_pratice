5d70bc90b6d7eb756229f3e96e6445682c44e86b
Liaunery <linjiajun@linjiajunde-MacBook-Pro.local>
2018-08-07 08:00:02 +0800

    first commit

---
+++ Pratice_1/Pratice_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Pratice_1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string a=Console.ReadLine();
+            Console.WriteLine(Convert.ToInt32(a,16));
+        }
+    }
+}
