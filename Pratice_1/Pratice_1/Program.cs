dac6299e454058a661838c4eb71690f5c05869c0
Liaunery <314804978@qq.com>
2018-08-09 22:48:24 +0800

    fourth commit

--- Pratice_1/Pratice_1/Program.cs
+++ Pratice_1/Pratice_1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Pratice_1
 {
@@ -9,48 +10,80 @@ namespace Pratice_1
             string a=Console.ReadLine();
             Console.WriteLine(Convert.ToInt32(a,16));
           */
-           /* //pratice_2
-        int[] a = { 1, 3, -5, 4 };
-        int[] b = { 1, 4, -5, -2 };
-            int d = 0;
-            foreach(int c in a){
-                Console.WriteLine(c*b[d]);
-                d++;
-            }
-            */
-            /*//pratice_3
-            string a=Console.ReadLine();
-            int b = System.Convert.ToInt32(a);
-            if(b%3==0){
-                Console.WriteLine("是三的倍数");
-            }
-            else if(b%7==0){
-                Console.WriteLine("是七的倍数");
+         /* //pratice_2
+      int[] a = { 1, 3, -5, 4 };
+      int[] b = { 1, 4, -5, -2 };
+          int d = 0;
+          foreach(int c in a){
+              Console.WriteLine(c*b[d]);
+              d++;
+          }
+          */
+         /*//pratice_3
+         string a=Console.ReadLine();
+         int b = System.Convert.ToInt32(a);
+         if(b%3==0){
+             Console.WriteLine("是三的倍数");
+         }
+         else if(b%7==0){
+             Console.WriteLine("是七的倍数");
+         }
+         else{
+             Console.WriteLine("不是3和7的倍数");
+
+         }
+         */
+         /*//pratice_4
+         string a=Console.ReadLine();
+         string[] b = a.Split(' ');
+         Console.WriteLine(b[0]=="Hello");
+         */
+         /*//pratice_5
+         string a=Console.ReadLine();
+         Console.WriteLine(a.Substring(1,2).Equals("HP")?a.Remove(1,2):"flase");
+         */
+         /*//pratice_6
+         Console.WriteLine("输入第一个整数：");
+         int a=Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine("输入第二个整数：");
+         int b = Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine("输入第三个整数：");
+         int c = Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine("最大:"+Math.Max(Math.Max(a,b),c));
+         Console.WriteLine("最小:"+Math.Min(Math.Min(a, b), c));
+         */
+         /*//pratice_7
+            Console.WriteLine("输入第一个整数");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("输入第一个整数");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a == b ? 0 : Math.Abs(a - 20) < Math.Abs(b - 20) ? a : b);
+        */
+        /*//pratice_8
+            Console.WriteLine("输入字符串：");
+            string str= Console.ReadLine();
+            int b=str.Count(a => a == 'w');
+            Console.WriteLine(b >= 1 &&b<= 3);
+        */
+        /*//pratice_9
+            Console.WriteLine("输入字符串：");
+            string str= Console.ReadLine();
+            if(str.Length<=3){
+                Console.WriteLine(str.ToUpper());
             }
             else{
-                Console.WriteLine("不是3和7的倍数");
-
+                Console.WriteLine(str.Substring(0,4).ToLower()+str.Substring(4));
             }
-            */
-            /*//pratice_4
-            string a=Console.ReadLine();
-            string[] b = a.Split(' ');
-            Console.WriteLine(b[0]=="Hello");
-            */
-            /*//pratice_5
-            string a=Console.ReadLine();
-            Console.WriteLine(a.Substring(1,2).Equals("HP")?a.Remove(1,2):"flase");
-            */
-            /*//pratice_6
-            Console.WriteLine("输入第一个整数：");
-            int a=Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("输入第二个整数：");
-            int b = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("输入第三个整数：");
-            int c = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("最大:"+Math.Max(Math.Max(a,b),c));
-            Console.WriteLine("最小:"+Math.Min(Math.Min(a, b), c));
-            */
+        */
+        /*//pratice_10
+            Console.WriteLine("输入字符串：");
+            string str=Console.ReadLine();
+            string sttr = "";
+            for (int i = 0; i < str.Length;i+=2){
+                sttr += str[i];
+            }
+            Console.WriteLine(sttr);
+        */
         }
     }
 }
