e7013ee0a711d11727fafc3c6842a66fee12c25c
Liaunery <314804978@qq.com>
2018-08-10 23:55:33 +0800

    fifth commit

--- Pratice_1/Pratice_1/Program.cs
+++ Pratice_1/Pratice_1/Program.cs
@@ -59,31 +59,60 @@ namespace Pratice_1
             int b = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine(a == b ? 0 : Math.Abs(a - 20) < Math.Abs(b - 20) ? a : b);
         */
-        /*//pratice_8
-            Console.WriteLine("输入字符串：");
-            string str= Console.ReadLine();
-            int b=str.Count(a => a == 'w');
-            Console.WriteLine(b >= 1 &&b<= 3);
+         /*//pratice_8
+             Console.WriteLine("输入字符串：");
+             string str= Console.ReadLine();
+             int b=str.Count(a => a == 'w');
+             Console.WriteLine(b >= 1 &&b<= 3);
+         */
+         /*//pratice_9
+             Console.WriteLine("输入字符串：");
+             string str= Console.ReadLine();
+             if(str.Length<=3){
+                 Console.WriteLine(str.ToUpper());
+             }
+             else{
+                 Console.WriteLine(str.Substring(0,4).ToLower()+str.Substring(4));
+             }
+         */
+         /*//pratice_10
+             Console.WriteLine("输入字符串：");
+             string str=Console.ReadLine();
+             string sttr = "";
+             for (int i = 0; i < str.Length;i+=2){
+                 sttr += str[i];
+             }
+             Console.WriteLine(sttr);
+         */
+         /*//praticec_11
+             int[] nums = { 1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1 };
+             int sum = 0;
+             foreach(int a in nums){
+                 sum += a;
+             }
+             Console.WriteLine("总和为："+sum);
+         */
+         /*//pratice_12
+            int[] a = { 1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1 };
+            int[] b = { 1, 2, 2, 3,3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 5 }; 
+            Console.WriteLine(a[0]==b[0]||a[a.Length-1]==b[b.Length-1]);
         */
-        /*//pratice_9
-            Console.WriteLine("输入字符串：");
-            string str= Console.ReadLine();
-            if(str.Length<=3){
-                Console.WriteLine(str.ToUpper());
+         //pratice_13
+            int[] a = { 1, 2, 8 };
+            int[] f =new int[3] ;
+            for (int c = a.Length - 1; c < 0; c--)
+            {
+                if(c-1<0){
+                    f[a.Length - 1] = a[0];
+                    continue;
+                }
+                f[c - 1] = a[c];
             }
-            else{
-                Console.WriteLine(str.Substring(0,4).ToLower()+str.Substring(4));
+            foreach(int e in f){
+                Console.WriteLine(e);
             }
-        */
-        /*//pratice_10
-            Console.WriteLine("输入字符串：");
-            string str=Console.ReadLine();
-            string sttr = "";
-            for (int i = 0; i < str.Length;i+=2){
-                sttr += str[i];
-            }
-            Console.WriteLine(sttr);
-        */
+
+
         }
     }
 }
