3d9ab8fbff8098277c8df868c5ae544eaf54499e
Liaunery <314804978@qq.com>
2018-08-17 22:31:17 +0800

    P2 First Commit

---
+++ Pratice_2/Pratice_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Pratice_2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //C# Data Types pratices
+            /*//pratice_1
+            string[] a = new string[3];
+            Console.WriteLine("第一个字符");
+             a[0]=Console.ReadLine();
+            Console.WriteLine("第二个字符");
+             a[1] = Console.ReadLine();
+            Console.WriteLine("第三个字符");
+             a[2] = Console.ReadLine();
+            System.Array.Reverse(a);
+            foreach(string b in a){
+                Console.WriteLine($"{b} ");
+            }
+            */  
+        }
+    }
+}
