266795578b3ebfd347b4ee224fb820fc090feca5
Liaunery <314804978@qq.com>
2018-09-02 21:23:03 +0800

    P5 first commit

---
+++ Praitice_5/Praitice_5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Praitice_5
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //P1. Write a program in C# Sharp to input a string and print it.
+            Console.WriteLine("输入字符串:");
+            Console.WriteLine($"你输入的字符:{Console.ReadLine()}");
+        }
+    }
+}
