7586bed36019f6d731642434879db957ac9301af
Liaunery <314804978@qq.com>
2018-09-03 21:09:10 +0800

    P5 second commit

--- Praitice_5/Praitice_5/Program.cs
+++ Praitice_5/Praitice_5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Praitice_5
 {
@@ -6,9 +7,35 @@ namespace Praitice_5
     {
         public static void Main(string[] args)
         {
-            //P1. Write a program in C# Sharp to input a string and print it.
+           /* //P1. Write a program in C# Sharp to input a string and print it.
             Console.WriteLine("输入字符串:");
             Console.WriteLine($"你输入的字符:{Console.ReadLine()}");
+
+            //P2.Write a program in C# Sharp to find the length of a string without using library function. 
+            Console.WriteLine("输入字符串:");
+            Array str=Console.ReadLine().ToCharArray();
+            int len=0;
+            foreach(char s in str){
+                len++;
+            }
+            Console.WriteLine($"字符串长度为:{len}");
+
+
+            //P3Write a program in C# Sharp to separate the individual characters from a string. 
+            Console.WriteLine("输入字符串:");
+            Array str = Console.ReadLine().ToCharArray();
+            foreach (char s in str)
+            {
+                Console.Write(s+" ");
+            }
+
+            Console.WriteLine("输入字符串:");
+            Array str =  Console.ReadLine().Reverse().ToArray();
+            foreach (char s in str)
+            {
+                Console.Write(s + " ");
+            }
+            */
         }
     }
 }
