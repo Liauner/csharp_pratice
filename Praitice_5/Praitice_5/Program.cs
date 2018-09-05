e412e42f212806929ca47a296acbe0686159dc07
Liaunery <314804978@qq.com>
2018-09-05 19:03:37 +0800

    P5 fourth commit

--- Praitice_5/Praitice_5/Program.cs
+++ Praitice_5/Praitice_5/Program.cs
@@ -35,7 +35,7 @@ namespace Praitice_5
             {
                 Console.Write(s + " ");
             }
-            */
+
 
             //P4.Write a program in C# Sharp to count a total number of vowel or consonant in a string.
             Console.WriteLine("输入字符串:");
@@ -49,6 +49,51 @@ namespace Praitice_5
             }
             Console.WriteLine($"元音字符有{yuan}个");
             Console.WriteLine($"辅音字符有{fu}个");
+
+
+            //P5.十进制转十六进制
+            Console.WriteLine("输入你想转化的十进制数:");
+            Console.WriteLine(toSixteen(Convert.ToInt32(Console.ReadLine()),""));
+
+
+            //P6.Write a program in C# Sharp to find maximum occurring character in a string.
+            Console.WriteLine("输入字符串:");
+            string a= Console.ReadLine();
+            var result = a.GroupBy(c => c).ToDictionary(c => c.Key, c => c.Count()).OrderByDescending(c=>c.Value);
+            foreach(var str in result){
+                Console.WriteLine(str.Key);
+                Console.WriteLine(str.Value);
+                break;
+            };
+
+            */
+
+
+
+        }
+        public static string toSixteen(int ten,string sixten){
+            if(ten/16==0){
+                if(ten%16==0){
+                    return sixten;
+                }else{
+                     sixten = tos(ten % 16) + sixten;
+                    return sixten;
+                }
+            }
+
+                int a = ten / 16;
+                int b = ten % 16;
+            sixten = tos(b) + sixten;
+            return toSixteen(a, sixten);
+        }
+
+        public static string tos(int number){
+            if(number<=9 && number>=0){
+                return number.ToString();
+            }else{
+                return Convert.ToChar(('A' + (number - 10))).ToString();
+            }
+
         }
     }
 }
